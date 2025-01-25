0a14611d8f5d1560d18c6f1e615267b3f0536224
Gleb <dyachenko.gleb.a@gmail.com>
2025-01-25 12:28:58 +0300

    Update main.cpp
    Обозначине строк и столбцов для поля, добавление формы фишек. Банан

--- main.cpp
+++ main.cpp
@@ -1,45 +1,61 @@
 #include <iostream> 
-#include <random> 
 
 using namespace std;  
 
 enum chip // здесь лежат все варианты фишек
 {
-	RED,
-	YELLOW,
-	GREEN,
-	BLUE,
-	VIOLET
+	RED = '*',
+	ORANGE = '$',
+	YELLOW = '&',
+	GREEN = '#',
+	BLUE = '0',
+	VIOLET = '@'
 };
 
-const char width = 20, height = 10; // Размеры поля
+const char width = 20, height = 10;
+chip field[height][width];
 
-char field[height][width]; // Двумерный массив с полем
-
-chip fishka(){
-	chip this_chip = random(0, 4);
-	return this_chip;
+chip make_chip(){
+	char this_chip = rand() % 6;
+	switch (this_chip)
+	{
+		case 0:
+			return RED;
+		case 1:
+			return ORANGE;
+		case 2:
+			return YELLOW;
+		case 3:
+			return GREEN;
+		case 4:
+			return BLUE;
+	}
+	return VIOLET;
 }
 
 void field_filling(){ 
-    for (char i = 0; i < height; i++) 
-        fill(field[i], field[i] + width, '0'); 
+    for (char y = 0; y < height; y++) 
+        for (char x = 0; x < width; x++)
+			field[y][x] = make_chip();
 } 
 
 void draw(){
-	cout << " "; 
-  	for (char i = 0; i < width; i++)
-		cout << "_"; 
+	cout << ' ';
+	for (char i = 0; i < width; i++)
+		cout << "_";
 	cout << "\n"; 
 	for (char i = 0; i < height; i++) { 
-		cout << "|"; 
+		printf("%d%c", i, '|');
 		for (char j = 0; j < width; j++)
-			cout << field[i][j];
+			cout << static_cast<char>(field[i][j]);
 		cout << "|\n"; 
 	}  
-	cout << " ";
+	cout << "  ";
 	for (char i = 0; i < width; i++)
 		cout << "‾"; 
+	cout << "\n  "; 
+  	for (char i = 0; i < width; i++)
+		cout << static_cast<char>(i + 'a'); 
 	cout << "\n"; 
 }
 
